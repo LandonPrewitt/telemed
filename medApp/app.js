ae01c9d8278249844fc4c884799eb66b7abc9803
landon <ltp368@tamu.edu>
2017-04-05 12:13:54 -0500

    Orginizing directories in repository

---
+++ medApp/app.js
@@ -0,0 +1,208 @@
+var express = require("express"),
+	app = express(),
+	server = require("http").createServer(app),
+	io = require("socket.io").listen(server),
+	mongoose = require('mongoose'),
+	users_server = {};
+
+server.listen(3000);
+
+// Mongo DB Code ================================================
+
+mongoose.connect('mongodb://localhost/', function(err){
+	if(err){
+		console.log(err);
+	} else {
+		console.log('Connected to mongodb!');
+	}
+});
+
+var userSchema = mongoose.Schema({
+	nick:String,
+	pw:String,
+	name:String,
+	email:String,
+	phone:String,
+	isDoctor:Boolean,
+	created: {type: Date, default: Date.now}
+});
+
+var chatSchema = mongoose.Schema({
+	nick:String,
+	msg: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Chat = mongoose.model('Message', chatSchema); 
+var User = mongoose.model('User', userSchema);
+
+// App Page Request Handlers ====================================
+
+app.use(express.static('public'))
+
+app.get('/', function(req,res){
+	res.sendfile(__dirname + "/index.html");
+})
+
+app.get('/conference', function(req,res){
+	res.sendfile(__dirname + "/public/conference.html");
+})
+
+app.get('/Logo.png', function(req,res){
+	res.sendfile(__dirname + "/public/images/Logo.png");
+})
+
+app.get('/test', function(req,res){
+	res.sendfile(__dirname + "/public/test.html");
+})
+
+app.get('/test2', function(req,res){
+	res.sendfile(__dirname + "/public/test2.html");
+})
+
+// Socket.io Handles ============================================
+
+io.sockets.on('connection', function(socket){
+
+
+	// io Function for updating usernames
+	function updateNicknames() {
+		io.sockets.emit('usernames', {users:Object.keys(users_server), user: socket.nickname});
+	}
+
+	// When user connects, load messages from db
+	Chat.find({}, function(err, docs){
+		if(err) throw err;
+		console.log("Sending old messages");
+		socket.emit('load old messages', docs);
+	});
+
+	// Event handler for recieving messages from client
+	socket.on('send message', function(data, callback){
+
+		var msg = data.trim();
+		if(msg.substr(0,3) == '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind != -1){
+				var name = msg.substr(0, ind);
+				var msg = msg.substr(ind + 1);
+				if(name in users_server) {
+					// Whisper to name socket
+					console.log("Whisper!");
+					users_server[name].emit('whisper', {msg: msg, nick: socket.nickname})
+				} else {
+					callback("Error! User doesn't exist.");
+				}
+			} else {
+				callback("Error! Please enter a message for your whisper.");
+			}
+		} else {
+			var newMsg = new Chat({msg: msg, nick: socket.nickname});
+			newMsg.save(function(err){
+				if(err) throw err;
+				console.log("message sender: " + socket.nickname);
+				io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+			});
+		}
+	});
+
+	// Event handler for when a client attempts to log in
+	socket.on('login', function(data, callback){
+			
+		// Create a query for attaining all usernames in mongo db
+		var cursor = User.find({}).cursor();
+		var dup = false;
+		var pwCheck = false;
+		cursor.on('data', function(doc){
+			
+			// Check for duplication of username
+			if (data.nick == doc.nick) {
+				dup = true;
+				if(data.pw == doc.pw) {
+					pwCheck = true;
+				}
+			}
+		});
+
+		cursor.on('close', function(){
+			if(data.nick in users_server) {
+				// Check to see if user is already existing online in a socket
+				callback({name: false, pass: true, err:"Already logged in"});
+				console.log("Name already exists");
+			} else if(!dup) {
+				callback({name: true, pass: false, err:"Username doesn't exist"});
+				console.log("No pass entered");
+			} else if(!pwCheck) {
+				callback({name: true, pass: false, err:"Password is incorrect!"});
+			} else {
+				callback({name: true, pass: true, err:""});
+				//if(err) throw err;
+				socket.nickname = data.nick;
+				users_server[socket.nickname] = socket;
+				updateNicknames();
+				console.log("Logged in!");
+			}
+		})	
+
+	});
+
+	// Handle when http requests to register user
+	socket.on('register user', function(data, callback){
+		
+		// Create a query for attaining all usernames in mongo db
+		var cursor = User.find({}).cursor();
+		var dup = false;
+		cursor.on('data', function(doc){
+			if (data.nick == doc.nick) {
+				dup = true;
+			}
+		});
+
+		cursor.on('close', function(){
+			if(data.nick in users_server || dup) {
+				// Check to see if user is already existing online in a socket
+				callback({name: false, pass: true, err:""});
+				console.log("Name already exists");
+			} else if(data.pw == '') {
+				callback({name: true, pass: false, err:""});
+				console.log("No pass entered");
+			} else {
+				callback({name: true, pass: true, err:""});
+				var newUsr = new User({
+					nick: data.nick, 
+					pw: data.pw, 
+					name:data.name, 
+					email:data.email, 
+					phone:data.phone,
+					isDoctor:data.doctor
+				});
+				newUsr.save(function(err){
+					if(err) throw err;
+					socket.nickname = data.nick;
+					users_server[socket.nickname] = socket;
+					updateNicknames();
+				});
+			}
+		})	
+	});
+
+	socket.on('clear history', function(){
+		Chat.remove({}, function(err){
+			if(err) throw err;
+			console.log("Messages erased by: " + socket.nickname);
+			
+		});
+	});
+
+	socket.on("disconnect", function(data){
+		if(!socket.nickname) return;
+		delete users_server[socket.nickname];
+		updateNicknames();
+	});
+
+	socket.on('update socket', function(data){
+		socket.nickname = data;
+	});
+
+});
---
+++ medApp/public/javascript.js
@@ -0,0 +1,144 @@
+jQuery(function($){
+	
+	// Declare variables
+	var socket = io.connect();
+	var $messageForm = $('#send-message');
+	var $clear = $('#clear');
+	var $messageBox =$('#message');
+	var $chat = $('#chat');
+	var $nickForm = $('#setNick');
+	var $errorMsg = $('#errorMsg');
+	var $nickBox = $('#nickname');
+	var $password = $('#password');
+	var $nickBox2 = $('#nickname2');
+	var $password2 = $('#password2');
+	var $users = $('#users');
+	var $login = $('#login_btn');
+	var $register = $('#register_btn');
+	var $getStarted = $('#getStarted_btn');
+	var $chat_btn = $('chat_btn');
+	var $myName = '';
+
+	// Handle the clear chat history button
+	$clear.click(function(e){
+		e.preventDefault();
+		socket.emit('clear history');
+		$chat.html('');
+	});
+
+	// Hande the action for when a user defines their nickname and submits
+	$login.click(function(e){
+		e.preventDefault();
+		socket.emit('login', {nick: $nickBox.val(), pw: $password.val()}, function(data){
+			if(data.err != ''){
+				$errorMsg.html('<span class="error">' + data.err + '</span>');
+			} else {
+				$('#nickWrap').hide();
+				//window.open("/conference","_self")
+				//$('#contentWrap').show();
+				$('#mainmenuWrap').show();
+			}
+		});
+		$myName = $nickBox.val();
+		$nickBox.val('');
+		$password.val('');
+	});
+
+	// Handle the action for registering a user *register_btn clicked*
+	$register.click(function(e){
+		e.preventDefault();
+		
+		$('#nickWrap').hide();
+		$('#registerWrap').show();
+
+	});
+
+	// Handle when the user submits their registration
+	$getStarted.click(function(e){
+		socket.emit('register user', {
+			nick: $nickBox2.val(), 
+			pw: $password2.val(), 
+			name: $('#name').val(), 
+			email: $('#email').val(), 
+			phone: $('#phone').val(),
+			doctor: $('#doctorBox').is(':checked')}, 
+			function(data){
+				if(!data.name){
+					$errorMsg.html('<span class="error">That username is already taken!</span>');
+				} else if(!data.pass) {
+					$errorMsg.html('<span class="error">Enter a valid password!</span>');
+				} else {
+					$('#registerWrap').hide();
+					//window.open("/conference","_self")
+					//$('#contentWrap').show();
+					$('#mainmenuWrap').show();
+					//socket.emit('update socket', $nickBox.val());
+				}
+			});
+		$myName = $nickBox2.val();
+		$nickBox.val('');
+		$password.val('');
+	});
+
+	// Handle when chat is opened
+	$('#chat_btn').click(function(e){
+		e.preventDefault();
+		$('#mainmenuWrap').hide();
+		$('#contentWrap').show();
+	});
+
+	// Hanle when chatBack_btn is clicked
+	$('#chatBack_btn').click(function(e){
+		e.preventDefault();
+		$('#mainmenuWrap').show();
+		$('#contentWrap').hide();
+	});
+
+	// Handle the function for when message is being sent
+	$('#message').keyup(function(e){
+		if(e.keyCode == 13){
+			$('#sendMsg').click();
+		}})
+	$('#sendMsg').click(function(e){
+		e.preventDefault();
+		socket.emit('send message', $messageBox.val(), function(data){
+			$chat.append('<span class=\"error"><b>' + data + "</span></br>");
+		});
+		$messageBox.val('');});
+
+	// Handling sending the message data to the client
+	socket.on('new message', function(data){
+		displayMsg(data);
+	});
+
+	// Handle the generation of the usernames
+	socket.on('usernames', function(data){
+		var html = '';
+		for(var i=0; i<data.users.length; i++){
+			if(data.users[i] == $myName) {
+				html += "<span style='color:green'>" + data.users[i] + "</span><br/>"
+			} else {
+				html += data.users[i] + '<br/>';
+			}
+		}
+
+		$users.html(html);
+	})
+
+	// Handle a whisper message send from server
+	socket.on('whisper', function(data){
+		$chat.append('<span class=\"whisper"><b>' + data.nick + ': </b>' + data.msg + "</span></br>");
+	});
+
+	socket.on('load old messages', function(docs){
+		for(var i=0; i<docs.length; i++){
+			displayMsg(docs[i]);
+		}
+	});
+
+	function displayMsg(data){
+		$chat.append('<span class=\"msg"><b>' + data.nick + ': </b>' + data.msg + "</span></br>");
+		$chat.scrollTop($chat.prop('scrollHeight'));
+	}
+
+});
