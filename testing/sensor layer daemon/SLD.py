72f7139fb3ee93c667368981e3e39612d0bc8322
Daniel <dmedi2@tamu.edu>
2017-04-11 22:13:56 -0500

    Improved ecg capture rate and more work in the SLD

--- testing/sensor layer daemon/SLD.py
+++ testing/sensor layer daemon/SLD.py
@@ -1,42 +1,48 @@
-import ardSensor, blueOxiSensor, blueBPSensor, blueSCSensor, errorcodes, pika, threading, json, sys
+import ardSensor, blueOxiSensor, blueBPSensor, blueSCSensor, errorcodes, pika, threading, json, sys, serial, pygatt
 from time import sleep
 
 
-
 def main(argv):
-    if len(argv) != 2:
-        print "Usage: %s server "%argv[0]
-        return 1
     
-    # Initializing continous measuring sensors
-    #try:
-     #   tempSensor = ardSensor.ArdSensor("t")
-    #    ecgSensor = ardSensor.ArdSensor("e")
- #  except:
-     #   pass
-        #TODO implement exception handler for arduino not connected
-        #print "Error connecting to arduino interface"
-        #publishError(channel, 1)
-   # try:
-    #    oxiSensor = blueOxiSensor.BlueOxiSensor()
-   # except pygatt.exceptions.NotConnectedError:
-    #    print "Error connecting to oximeter module"
-    #    publishError(channel, 1)
+    if len(argv) != 2:
+        print "Usage: %s AMQPURL "%argv[0]
+        return 1
     
     # Connecting and setting up rabbitmq message broker
     connection = pika.BlockingConnection(pika.URLParameters(argv[1]))
     channel = connection.channel()
-    queues = ["error", "cmd", "temp", "ecg", "oxi", "hr", "pi", "sysbp", "diabp", "pulse", "weight"]
+    channel.queue_declare(queue='error')
+    
+    # Initializing continous measuring sensors
+    connected_sensors = {'temp': False , 'ecg': False, 'oxi': False}
+    try:
+       tempSensor = ardSensor.ArdSensor("t")
+       ecgSensor = ardSensor.ArdSensor("e")
+       connected_sensors['temp'] = True
+       connected_sensors['ecg'] = True
+    except serial.serialutil.SerialException:
+        print "Error connecting to arduino interface"
+        tempSensor = None
+        ecgSensor = None
+        publishError(channel, 1)
+    try:
+       oxiSensor = blueOxiSensor.BlueOxiSensor()
+       connected_sensors['oxi'] = True
+    except pygatt.exceptions.NotConnectedError:
+       print "Error connecting to oximeter module"
+       oxiSensor = None
+       publishError(channel, 1)
+    
+    # Declaring rest of queues
+    queues = ["cmd", "temp", "ecg", "oxi", "hr", "pi", "bp", "diabp", "pulse", "weight"]
     for qstr in queues:
         channel.queue_declare(queue=qstr)
-    
+        
     try:
         # Starting thread for producer of sensors with continous data
-        #contProdWorker = threading.Thread(target=contProducer, args=(channel, tempSensor, ecgSensor, oxiSensor))
-        contProdWorker = threading.Thread(target=contProducer, args=(channel,))
+        contProdWorker = threading.Thread(target=contProducer, args=(channel, connected_sensors, tempSensor, ecgSensor, oxiSensor))
         contProdWorker.daemon = True
         contProdWorker.start()
-        #while True: sleep(100)
         # Listening for commands
         print "Listening for commands..."
         channel.basic_consume(executeCMD, queue="cmd", no_ack=True)
@@ -64,47 +70,48 @@ def executeCMD(channel, method, header, body):
             singleMeasureWorker.start()
     else:
         publishError(channel, 2)
-        
+
+# Worker definition for single measurements        
 def singleMeasureProd(channel, sensor):
+    
     if sensor == "bp":
         print "Rcvd command and starting blood pressure measurement procedure.."
         bpSensor = blueBPSensor.BlueBPSensor()
+        print "Measuring..."
         (sysbp, diabp, pulse) = bpSensor.startMeasure()
-        channel.basic_publish(exchange='', routing_key="sysbp", body=str(sysbp))
-        #channel.basic_publish(exchange='', routing_key="diabp", body=diabp)
-        #channel.basic_publish(exchange='', routing_key="pulse", body=pulse)
-        print "Measurement sent"
+        channel.basic_publish(exchange='', routing_key="bp", body=json.JSONEncoder().encode([sysbp, diabp, pulse]))
+        print "Received data from blood pressure sensor: sysbp: %i diabp: %i pulse: %i and sucessfully sent to queue" % (sysbp, diabp, pulse)
 
     elif sensor == "sc":
         print "Rcvd command and starting weight measurement procedure.."
         scSensor = blueSCSensor.BlueSCSensor()
-        weight = scSensor.startMeasure()
-        channel.basic_publish(exchange='', routing_key="sc", body=str(round(weight*2.20462/10,1)))
+        print "Measuring..."
+        weight = round(scSensor.startMeasure()*2.20462/10,1)
+        channel.basic_publish(exchange='', routing_key="weight", body=str(weight))
+        print "Received data from scale: weight: %i and sucessfully sent to queue" % (weight)
     
     else:
         publishError(channel, 3)
     
-    
+
+# Helper for sending defined error codes to the server    
 def publishError(channel, errorn):
     print "Error:", errorcodes.error_desc[errorn]
     channel.basic_publish(exchange='', routing_key="error", body=errorcodes.error[errorn])
+    channel.basic_publish(exchange='', routing_key="error", body=errorcodes.error_desc[errorn])
     
         
 # Producing sensor data messages for sensors outputting continuous data 
-#def contProducer(channel, tempSensor, ecgSensor, oxiSensor):    
-def contProducer(channel):  
+def contProducer(channel, connected_sensors, tempSensor, ecgSensor, oxiSensor):    
     while(True):
-        #readings = json.JSONEncoder().encode({"lengths":lengths, "alt":alt})
-        #channel.basic_publish(exchange='', routing_key="temp", body=tempSensor.getMeasure())
-         channel.basic_publish(exchange='', routing_key="ecg", body=JSONEncoder().encode(ecgSensor.getMeasureArr(10)))
-        #channel.basic_publish(exchange='', routing_key="oxi", body=str(oxiSensor.getMeasureOxi()))
-        #channel.basic_publish(exchange='', routing_key="hr", body=str(oxiSensor.getMeasureHR()))
-        #channel.basic_publish(exchange='', routing_key="pi", body=str(oxiSensor.getMeasurePI()))
-	    #channel.basic_publish(exchange='', routing_key="pi", body="u suck")
-        print("Five sensors decoded data sent", channel)
-        sleep(1)
-        
+        if connected_sensors['temp'] and connected_sensors['ecg']:
+            channel.basic_publish(exchange='', routing_key="temp", body=tempSensor.getMeasure())
+            channel.basic_publish(exchange='', routing_key="ecg", body=json.JSONEncoder().encode(ecgSensor.getMeasureArr(10)))
+        if connected_sensors['oxi']:
+            channel.basic_publish(exchange='', routing_key="oxi", body=json.JSONEncoder().encode([oxiSensor.getMeasureOxi(), oxiSensor.getMeasureHR(), oxiSensor.getMeasurePI()]))
     
+        print("Sending continous sensors' data", channel)
+        sleep(1)
         
 
 if __name__ == '__main__':
